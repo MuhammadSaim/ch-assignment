8d7f453b62022b00b6ba9bd75bd7045b6e2d661d
Muhammad Saim <muhammadsaim494@gmail.com>
2021-10-01 09:37:50 +0500

    Add and configure tailwindcss and alpinejs with laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,18 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+// configure fonts and images files in the main assets directory not in public
+mix.options({
+    fileLoaderDirs: {
+        fonts: "assets/fonts",
+        images: "assets/images",
+    },
+});
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js("resources/js/app.js", "public/assets/js").postCss(
+    "resources/css/app.css",
+    "public/assets/css",
+    [require("tailwindcss")]
+);
+
+// disable build notifications
+mix.disableNotifications();
